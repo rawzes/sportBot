7bcf5d5bc42dd0eb224e42ea279b2d01b87c58e1
rawzes <rawzes@gmail.com>
2023-01-23 21:34:01 +0300

    added new handlers

---
+++ data.py
@@ -0,0 +1,29 @@
+import pytz
+import requests
+from datetime import datetime
+
+URL1 = 'https://d.flashscore.com/x/feed/fp_37_'
+OFFSET = 0
+URL2 = '_3_en_1'
+TIMEZONE = 'Europe/Minsk'
+EMPTY = ';1:'
+
+
+async def get_data(offset=0):
+    current_url = URL1 + str(offset) + URL2
+    headers = {"x-fsign": "SW9D1eZo"}
+    r = requests.get(url=current_url, headers=headers)
+    result = r.text
+    pairs = []
+    if EMPTY in result:
+        return pairs
+    result = result[10:]
+    list_values = result.split('¬~ZA÷')
+
+    for item in list_values:
+        tmp = item.split('¬ZN÷')
+        event_title = tmp[0].split('¬ZEE÷')[0]
+        event_date = datetime.fromtimestamp(int(tmp[1].split('|¬ZSS÷')[0]), pytz.timezone(TIMEZONE))
+        pairs.append(event_date.strftime('%Y-%m-%d %H:%M:%S') + " - " + event_title)
+
+    return pairs
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import messages
-
+import data
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,8 +15,33 @@ load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
 
 
+async def get_events(day=0):
+    events = await data.get_data(day)
+    message = ''
+    if len(events) == 0:
+        message = 'На сегодня нет спортивных событий'
+    else:
+        message = ' '.join(events)
+    return message
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=messages.START)
+
+
+async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = await get_events(0)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = await get_events(1)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def after_tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = await get_events(2)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 if __name__ == '__main__':
@@ -24,5 +49,14 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    today_handler = CommandHandler('today', today)
+    application.add_handler(today_handler)
+
+    tomorrow_handler = CommandHandler('tomorrow', tomorrow)
+    application.add_handler(tomorrow_handler)
+
+    tomorrow_handler = CommandHandler('after_tomorrow', after_tomorrow)
+    application.add_handler(tomorrow_handler)
 
     application.run_polling()
